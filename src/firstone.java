31c9e14ca30652b5623111cfbaa4f9c82d6d3d62
Marsel <smurf.harb@gmail.com>
2024-03-10 10:39:38 +0300

    new commit

--- src/firstone.java
+++ src/firstone.java
@@ -1,5 +1,5 @@
 public class firstone {
     public static void main(String[] args) {
-        System.out.println("this is the first branch");
+        System.out.println("this is the first modification");
     }
 }
